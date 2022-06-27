7c541fa2865b043a57abc5871d63d968a747f168
kacper Hajduk <odynek666@wp.pl>
2022-06-27 17:01:51 +0200

    .

--- MusicPlayerTut/Properties/Resources.Designer.cs
+++ MusicPlayerTut/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MusicPlayerTut.Properties {
+namespace AppHub.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MusicPlayerTut.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MusicPlayerTut.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AppHub.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
